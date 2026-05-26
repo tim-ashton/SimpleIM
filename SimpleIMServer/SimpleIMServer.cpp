98a1f3d542669ee6dd08ea1410a4517da93b54b9
Tim Ashton <t.j.ashton1@gmail.com>
2026-05-26 16:17:40 +1000

    Add graceful shutdown handling in server main

--- SimpleIMServer/SimpleIMServer.cpp
+++ SimpleIMServer/SimpleIMServer.cpp
@@ -1,22 +1,33 @@
 #include "IncomingConnHandler.h"
 
-#include <iostream>
+#include <atomic>
 #include <chrono>
+#include <csignal>
+#include <iostream>
 #include <thread>
 
+namespace {
+std::atomic<bool> g_running{true};
 
-
+void stopServer(int) {
+    g_running = false;
+}
+} // namespace
 
 int main(int argc, char *argv[]) {
+    std::signal(SIGINT, stopServer);
+    std::signal(SIGTERM, stopServer);
 
     std::cout << "Starting SimpleIM Server" << std::endl;
-    
+
     IncomingConnHandler connectionHandler;
     connectionHandler.start();
 
-    while(true) {
+    while (g_running) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
+
+    connectionHandler.stop();
 
     return 0;
 }
